b89d3cb7d9438428428404447e3fc3eee77a3dc7
Joshua David <jshuadvd@icloud.com>
2015-05-24 23:14:50 -0700

    Login Screen now displays

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -25,9 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080001;
+        public static final int action_settings=0x7f080006;
+        public static final int loginButton=0x7f080002;
+        public static final int pager=0x7f080004;
+        public static final int passwordField=0x7f080001;
+        public static final int section_label=0x7f080005;
+        public static final int signUpText=0x7f080003;
+        public static final int usernameField=0x7f080000;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
