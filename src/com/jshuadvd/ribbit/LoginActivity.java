967b86b00cf6e7c1880fa84a4764b394f29aeb76
Joshua David <jshuadvd@icloud.com>
2015-05-25 21:15:28 -0700

    Added OnClickListener

--- src/com/jshuadvd/ribbit/LoginActivity.java
+++ src/com/jshuadvd/ribbit/LoginActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+	
+	protected TextView mSignUpTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		mSignUpTextView = (TextView)findViewById(R.id.signupButton);
 	}
 
 	@Override
