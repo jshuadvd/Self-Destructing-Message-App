dfa9a3d51f01204f32a1cd1c1388ec6d478b09fb
Joshua David <jshuadvd@icloud.com>
2015-05-25 21:07:18 -0700

    Fixing Sign Up Activity

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -25,12 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int emailField=0x7f080005;
         public static final int loginButton=0x7f080002;
         public static final int pager=0x7f080004;
         public static final int passwordField=0x7f080001;
-        public static final int section_label=0x7f080005;
+        public static final int section_label=0x7f080007;
         public static final int signUpText=0x7f080003;
+        public static final int signupButton=0x7f080006;
         public static final int usernameField=0x7f080000;
     }
     public static final class layout {
@@ -47,9 +49,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int email_hint=0x7f05000c;
         public static final int hello_world=0x7f050004;
         public static final int login_button_label=0x7f050009;
         public static final int password_hint=0x7f050008;
+        public static final int sign_up_button_label=0x7f05000d;
         public static final int sign_up_text=0x7f05000a;
         public static final int title_activity_login=0x7f050006;
         public static final int title_activity_sign_up=0x7f05000b;
