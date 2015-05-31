e22682cb28df737fdd7b83281e6b5334a710f793
Joshua David <jshuadvd@icloud.com>
2015-05-30 20:04:38 -0700

    Modifying the App tabs from the template

--- src/com/jshuadvd/ribbit/MainActivity.java
+++ src/com/jshuadvd/ribbit/MainActivity.java
@@ -21,7 +21,7 @@ import android.view.ViewGroup;
 import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
+public class MainActivity extends Activity  implements ActionBar.TabListener {
 	
 	public static final String TAG = MainActivity.class.getSimpleName();
 
@@ -137,44 +137,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 			FragmentTransaction fragmentTransaction) {
 	}
 
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a PlaceholderFragment (defined as a static inner class
-			// below).
-			return PlaceholderFragment.newInstance(position + 1);
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			}
-			return null;
-		}
-	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
