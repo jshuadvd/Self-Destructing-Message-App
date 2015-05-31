0ea1f451f259314f04226537b58cd73980126c36
Joshua David <jshuadvd@icloud.com>
2015-05-30 20:30:25 -0700

    Adding List View For teh Fragment

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -25,13 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_logout=0x7f080009;
-        public static final int action_settings=0x7f080008;
+        public static final int action_logout=0x7f080008;
+        public static final int action_settings=0x7f080007;
         public static final int emailField=0x7f080005;
         public static final int loginButton=0x7f080002;
         public static final int pager=0x7f080004;
         public static final int passwordField=0x7f080001;
-        public static final int section_label=0x7f080007;
         public static final int signUpText=0x7f080003;
         public static final int signupButton=0x7f080006;
         public static final int usernameField=0x7f080000;
