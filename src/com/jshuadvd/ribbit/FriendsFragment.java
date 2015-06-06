9d962402ea309d618b8abda288fa9190a95cdc4a
Joshua David <jshuadvd@icloud.com>
2015-06-05 21:56:02 -0700

    REturning the list of Friends

--- src/com/jshuadvd/ribbit/FriendsFragment.java
+++ src/com/jshuadvd/ribbit/FriendsFragment.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -41,7 +41,20 @@ public class FriendsFragment extends ListFragment {
 
 			@Override
 			public void done(List<ParseUser> friends, ParseException e) {
-			
+				mFriends = friends;
+
+				String[] usernames = new String[mFriends.size()]; 
+				int i = 0;
+				for(ParseUser user : mFriends) {
+					usernames[i] = user.getUsername();
+					i++;
+				}
+				
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+						getListView().getContext(), 
+						android.R.layout.simple_list_item_1, 
+						usernames);
+				setListAdapter(adapter);
 			}
 		});
 		
