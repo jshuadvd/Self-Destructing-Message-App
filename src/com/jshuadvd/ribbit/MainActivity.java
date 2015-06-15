144b97b44169053ffca28729cf1546b2199568bf
Joshua David <jshuadvd@icloud.com>
2015-06-14 22:29:08 -0700

    Adding new Intent to start the REcipientsActivity in MainActivity

--- src/com/jshuadvd/ribbit/MainActivity.java
+++ src/com/jshuadvd/ribbit/MainActivity.java
@@ -281,6 +281,13 @@ public class MainActivity extends FragmentActivity implements
 				mediaScanIntent.setData(mMediaUri);
 				sendBroadcast(mediaScanIntent);
 				}
+			
+			Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
+			recipientsIntent.setData(mMediaUri);
+			startActivity(recipientsIntent);
+			
+			
+			
 		}
 		else if (resultCode != RESULT_CANCELED) {
 			Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
