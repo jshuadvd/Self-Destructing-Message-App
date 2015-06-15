aad557dc174250c34634e6425402322450884528
Joshua David <jshuadvd@icloud.com>
2015-06-14 22:20:48 -0700

    Adding blocks to select the recipients

--- src/com/jshuadvd/ribbit/RecipientsActivity.java
+++ src/com/jshuadvd/ribbit/RecipientsActivity.java
@@ -1,17 +1,85 @@
 package com.jshuadvd.ribbit;
 
+import java.util.List;
+
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
 
 public class RecipientsActivity extends ListActivity {
+	
+	public static final String TAG = RecipientsActivity.class.getSimpleName();
+
+	protected ParseRelation<ParseUser> mFriendsRelation;
+	protected ParseUser mCurrentUser;	
+	protected List<ParseUser> mFriends;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_recipients);
 	}
+	
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		mCurrentUser = ParseUser.getCurrentUser();
+		mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+		
+		setProgressBarIndeterminateVisibility(true);
+		
+		ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+		query.addAscendingOrder(ParseConstants.KEY_USERNAME);
+		query.findInBackground(new FindCallback<ParseUser>() {
+			
+			@Override
+			public void done(List<ParseUser> friends, ParseException e) {
+				
+				setProgressBarIndeterminateVisibility(false);
+				
+				if (e == null) {
+					mFriends = friends;
+					
+					String[] usernames = new String[mFriends.size()];
+					int i = 0;
+					for(ParseUser user : mFriends) {
+						usernames[i] = user.getUsername();
+						i++;
+					}
+					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+							getListView().getContext(), 
+							android.R.layout.simple_list_item_1,
+							usernames);
+					setListAdapter(adapter);
+				}
+				else {
+					Log.e(TAG, e.getMessage());
+					AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+					builder.setMessage(e.getMessage())
+						.setTitle(R.string.error_title)
+						.setPositiveButton(android.R.string.ok, null);
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				}
+			}
+		});
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,4 +99,15 @@ public class RecipientsActivity extends ListActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+
+
+
+
+
+
 }
+
+
+
+
