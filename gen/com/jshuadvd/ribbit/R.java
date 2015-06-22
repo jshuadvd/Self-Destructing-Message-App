b57db5fed4b366b19173754770555fc262a79ee8
Joshua David <jshuadvd@icloud.com>
2015-06-21 18:59:00 -0700

    Adding padding for the message view

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -23,6 +23,7 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int message_list_item_vertical_margin=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_action_camera=0x7f020000;
