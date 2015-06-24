170109f01127010e7d1ea556d8e42a4f29ae91e5
Joshua David <jshuadvd@icloud.com>
2015-06-23 21:40:38 -0700

    Added conditional logic for the convertView

--- src/com/jshuadvd/ribbit/MessageAdapter.java
+++ src/com/jshuadvd/ribbit/MessageAdapter.java
@@ -27,13 +27,23 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
 		
-		convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
+			
+			// Initialize holder as new ViewHolder
+			holder = new ViewHolder();
+			holder.iconImageView = (ImageView)convertView.findViewById(R.id.messageIcon) ;
+			holder.nameLabel = (TextView)convertView.findViewById(R.id.senderLabel);
+		}
+		else {
+			holder = (ViewHolder).convertView.getTag();
+		}
 		
-		// Initialize holder as new ViewHolder
-		holder = new ViewHolder();
-		holder.iconImageView = (ImageView)convertView.findViewById(R.id.messageIcon) ;
-		holder.nameLabel = (TextView)convertView.findViewById(R.id.senderLabel);
-		return convertView;
+		ParseObject message = mMessages.get(position);
+		holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+		
+			return convertView;
+		
 	}
 	
 	private static class ViewHolder {
