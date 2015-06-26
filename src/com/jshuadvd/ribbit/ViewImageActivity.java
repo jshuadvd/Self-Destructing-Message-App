bfeb32e8d6c91c2a4cf70b7c1a35a4e9f4b267b2
Joshua David <jshuadvd@icloud.com>
2015-06-25 23:51:37 -0700

    Added TimerTask for the message to be destroyed

--- src/com/jshuadvd/ribbit/ViewImageActivity.java
+++ src/com/jshuadvd/ribbit/ViewImageActivity.java
@@ -1,6 +1,7 @@
 package com.jshuadvd.ribbit;
 
-import com.squareup.picasso.Picasso;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.net.Uri;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class ViewImageActivity extends Activity {
 
@@ -21,7 +24,16 @@ public class ViewImageActivity extends Activity {
 		Uri imageUri = getIntent().getData();
 		
 		Picasso.with(this).load(imageUri.toString()).into(imageView);
-	}
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				finish();
+				
+			}
+		}, 10 * 1000);
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
