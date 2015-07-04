8ee46a45d33dafd49eef64e9a730666295e5745c
Joshua David <jshuadvd@icloud.com>
2015-07-03 22:14:01 -0700

    Added Linear Layout for username and password bg

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -48,23 +48,24 @@ public final class R {
         public static final int ic_video=0x7f020010;
     }
     public static final class id {
-        public static final int action_camera=0x7f0a0010;
-        public static final int action_edit_friends=0x7f0a000e;
-        public static final int action_logout=0x7f0a000f;
-        public static final int action_send=0x7f0a0011;
-        public static final int action_settings=0x7f0a000d;
+        public static final int action_camera=0x7f0a0011;
+        public static final int action_edit_friends=0x7f0a000f;
+        public static final int action_logout=0x7f0a0010;
+        public static final int action_send=0x7f0a0012;
+        public static final int action_settings=0x7f0a000e;
         public static final int backgroundImage=0x7f0a0000;
-        public static final int emailField=0x7f0a0008;
-        public static final int imageView=0x7f0a000a;
-        public static final int loginButton=0x7f0a0002;
-        public static final int messageIcon=0x7f0a000b;
-        public static final int pager=0x7f0a0007;
-        public static final int passwordField=0x7f0a0003;
-        public static final int senderLabel=0x7f0a000c;
-        public static final int signUpText=0x7f0a0001;
-        public static final int signupButton=0x7f0a0009;
-        public static final int subTitle=0x7f0a0006;
-        public static final int title=0x7f0a0005;
+        public static final int editTextLayout=0x7f0a0003;
+        public static final int emailField=0x7f0a0009;
+        public static final int imageView=0x7f0a000b;
+        public static final int loginButton=0x7f0a0006;
+        public static final int messageIcon=0x7f0a000c;
+        public static final int pager=0x7f0a0008;
+        public static final int passwordField=0x7f0a0005;
+        public static final int senderLabel=0x7f0a000d;
+        public static final int signUpText=0x7f0a0007;
+        public static final int signupButton=0x7f0a000a;
+        public static final int subtitle=0x7f0a0002;
+        public static final int title=0x7f0a0001;
         public static final int usernameField=0x7f0a0004;
     }
     public static final class layout {
