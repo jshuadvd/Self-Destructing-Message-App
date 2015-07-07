741e2fed3021eb1622ac10574c0c258a4702bc7f
Joshua David <jshuadvd@icloud.com>
2015-07-06 22:31:47 -0700

    Extracted new style

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -164,9 +164,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080003;
+        public static final int AppBaseTheme=0x7f080004;
         public static final int AppTheme=0x7f080000;
         public static final int AuthBackground=0x7f080001;
         public static final int AuthBackgroundImage=0x7f080002;
+        public static final int AuthTitle=0x7f080003;
     }
 }
