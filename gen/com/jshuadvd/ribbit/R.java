a0107a8ed8f770ab49b169e089a5bd18feddd70a
Joshua David <jshuadvd@icloud.com>
2015-07-10 22:37:27 -0700

    Finalized Sign up And Cancel button

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -70,22 +70,25 @@ public final class R {
         public static final int ic_video=0x7f02001d;
     }
     public static final class id {
-        public static final int action_camera=0x7f0a0011;
-        public static final int action_edit_friends=0x7f0a000f;
-        public static final int action_logout=0x7f0a0010;
-        public static final int action_send=0x7f0a0012;
-        public static final int action_settings=0x7f0a000e;
+        public static final int action_camera=0x7f0a0014;
+        public static final int action_edit_friends=0x7f0a0012;
+        public static final int action_logout=0x7f0a0013;
+        public static final int action_send=0x7f0a0015;
+        public static final int action_settings=0x7f0a0011;
         public static final int backgroundImage=0x7f0a0000;
+        public static final int buttonDivider=0x7f0a000a;
+        public static final int cancelButton=0x7f0a000c;
         public static final int editTextLayout=0x7f0a0003;
         public static final int emailField=0x7f0a0009;
-        public static final int imageView=0x7f0a000b;
+        public static final int imageView=0x7f0a000d;
         public static final int loginButton=0x7f0a0006;
-        public static final int messageIcon=0x7f0a000c;
+        public static final int messageIcon=0x7f0a000f;
         public static final int pager=0x7f0a0008;
         public static final int passwordField=0x7f0a0005;
-        public static final int senderLabel=0x7f0a000d;
+        public static final int section_label=0x7f0a000e;
+        public static final int senderLabel=0x7f0a0010;
         public static final int signUpText=0x7f0a0007;
-        public static final int signupButton=0x7f0a000a;
+        public static final int signupButton=0x7f0a000b;
         public static final int subtitle=0x7f0a0002;
         public static final int title=0x7f0a0001;
         public static final int usernameField=0x7f0a0004;
@@ -100,7 +103,8 @@ public final class R {
         public static final int auth_common=0x7f030006;
         public static final int fragment_friends=0x7f030007;
         public static final int fragment_inbox=0x7f030008;
-        public static final int message_item=0x7f030009;
+        public static final int fragment_main_dummy=0x7f030009;
+        public static final int message_item=0x7f03000a;
     }
     public static final class menu {
         public static final int login=0x7f090000;
@@ -112,7 +116,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int cancel_button_label=0x7f060029;
         public static final int content_desc_background=0x7f060027;
+        public static final int content_desc_button_divider=0x7f06002a;
         public static final int email_hint=0x7f06000b;
         public static final int empty_friends_label=0x7f060013;
         public static final int empty_inbox_label=0x7f060012;
@@ -165,10 +171,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080006;
+        public static final int AppBaseTheme=0x7f080008;
         public static final int AppTheme=0x7f080000;
         public static final int AuthBackground=0x7f080001;
         public static final int AuthBackgroundImage=0x7f080002;
+        public static final int AuthButton=0x7f080007;
+        public static final int AuthEditText=0x7f080006;
         public static final int AuthFieldContainer=0x7f080005;
         public static final int AuthTitle=0x7f080003;
         public static final int AuthTitle_AuthSubTitle=0x7f080004;
