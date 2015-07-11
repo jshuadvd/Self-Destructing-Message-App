3164f15d7a04441e6e660e95f77ccabdcf9e0a74
Joshua David <jshuadvd@icloud.com>
2015-07-10 22:41:15 -0700

    Added Cancel button to SignUpActivity

--- src/com/jshuadvd/ribbit/SignUpActivity.java
+++ src/com/jshuadvd/ribbit/SignUpActivity.java
@@ -22,6 +22,7 @@ public class SignUpActivity extends ListActivity {
 	protected EditText mPassword;
 	protected EditText mEmail;
 	protected Button mSignUpButton;
+	protected Button mCancelButton;
 	
 	
 
@@ -37,6 +38,14 @@ public class SignUpActivity extends ListActivity {
 		mUsername = (EditText)findViewById(R.id.usernameField);
 		mPassword = (EditText)findViewById(R.id.passwordField);
 		mEmail = (EditText)findViewById(R.id.emailField);
+		mCancelButton = (Button)findViewById(R.id.cancelButton);
+		mCancelButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 		mSignUpButton = (Button)findViewById(R.id.signupButton);
 		
 		
