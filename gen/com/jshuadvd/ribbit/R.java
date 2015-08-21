f85b3e4546f4f51a493866662a34036c0634ac1b
Joshua David <jshuadvd@icloud.com>
2015-08-20 20:26:30 -0700

    Added checkImageView

--- gen/com/jshuadvd/ribbit/R.java
+++ gen/com/jshuadvd/ribbit/R.java
@@ -111,21 +111,22 @@ public final class R {
         public static final int tab_unselected_ribbit=0x7f02003d;
     }
     public static final class id {
-        public static final int action_camera=0x7f0a0019;
-        public static final int action_edit_friends=0x7f0a0017;
-        public static final int action_logout=0x7f0a0018;
-        public static final int action_send=0x7f0a001a;
-        public static final int action_settings=0x7f0a0016;
+        public static final int action_camera=0x7f0a001a;
+        public static final int action_edit_friends=0x7f0a0018;
+        public static final int action_logout=0x7f0a0019;
+        public static final int action_send=0x7f0a001b;
+        public static final int action_settings=0x7f0a0017;
         public static final int backgroundImage=0x7f0a0000;
         public static final int buttonDivider=0x7f0a000a;
         public static final int cancelButton=0x7f0a000c;
+        public static final int checkImageView=0x7f0a0015;
         public static final int editTextLayout=0x7f0a0003;
         public static final int emailField=0x7f0a0009;
         public static final int friendsGrid=0x7f0a0013;
         public static final int imageView=0x7f0a000d;
         public static final int loginButton=0x7f0a0006;
         public static final int messageIcon=0x7f0a0010;
-        public static final int nameLabel=0x7f0a0015;
+        public static final int nameLabel=0x7f0a0016;
         public static final int pager=0x7f0a0008;
         public static final int passwordField=0x7f0a0005;
         public static final int section_label=0x7f0a000f;
