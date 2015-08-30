71d9e97031a5a3613e4bac6bbf8d1cada1e77bd8
Joshua David <jshuadvd@icloud.com>
2015-08-29 23:55:57 -0700

    CHanged Application info

--- src/com/jshuadvd/ribbit/RibbitApplication.java
+++ src/com/jshuadvd/ribbit/RibbitApplication.java
@@ -10,7 +10,9 @@ public class RibbitApplication extends Application {
 	@Override
 	public void onCreate() { 
 		super.onCreate();
-	    Parse.initialize(this, "5mxmuCNaVB2ua1mrQRVmsNIDUg8KfcbYrlzzrulh", "tEpFDQp3SneukuBDXXem6GID6Tx56fJe83ZZjEMt");
+	    
+		Parse.initialize(this, "5mxmuCNaVB2ua1mrQRVmsNIDUg8KfcbYrlzzrulh", 
+	    						"tEpFDQp3SneukuBDXXem6GID6Tx56fJe83ZZjEMt");
 	    
 	    ParseInstallation.getCurrentInstallation().saveInBackground();
 	    
