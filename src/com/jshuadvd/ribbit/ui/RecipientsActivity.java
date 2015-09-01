30e57d5e2cd27d698eb1d049783adcb293b260ca
Joshua David <jshuadvd@icloud.com>
2015-08-31 22:17:58 -0700

    Added KEY_USER_ID as the key in the query

--- src/com/jshuadvd/ribbit/ui/RecipientsActivity.java
+++ src/com/jshuadvd/ribbit/ui/RecipientsActivity.java
@@ -261,7 +261,7 @@ public class RecipientsActivity extends Activity {
 	protected void sendPushNotifications() {
 		
 		ParseQuery<ParseInstallation> query = ParseInstallation.getQuery();
-		query.whereContainedIn(key, values)
+		query.whereContainedIn(ParseConstants.KEY_USER_ID, values)
 	}
 
 }
