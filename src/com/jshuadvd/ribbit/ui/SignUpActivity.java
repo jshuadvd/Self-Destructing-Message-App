1642c5ebb52c70068872520d2a85948eff6526ea
Joshua David <jshuadvd@icloud.com>
2015-09-03 17:35:30 -0700

    Added RibbitApplication.updateParseInstallation(user) to SignUpActivity

--- src/com/jshuadvd/ribbit/ui/SignUpActivity.java
+++ src/com/jshuadvd/ribbit/ui/SignUpActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.jshuadvd.ribbit.R;
+import com.jshuadvd.ribbit.RibbitApplication;
 import com.jshuadvd.ribbit.R.id;
 import com.jshuadvd.ribbit.R.layout;
 import com.jshuadvd.ribbit.R.string;
@@ -83,6 +84,9 @@ public class SignUpActivity extends Activity {
 							
 							if (e == null) {
 								// Success!
+								RibbitApplication.updateParseInstallation(user);
+								
+								
 								Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
 								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
